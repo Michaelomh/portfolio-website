719e136216aead20cb7a3f3843430afa25241c78
Michael Ong <minghao.ong.2014@sis.smu.edu.sg>
2022-07-24 21:42:56 +0800

    fix linting problems

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 import "@fontsource/jost";
 import "@fontsource/inter";
 
-import colors from "./colors";
+import { colors } from "./colors";
 import { fonts, textStyles } from "./typography";
 
 const theme = extendTheme({
