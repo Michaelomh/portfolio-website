e49e42e0201527d1641c41fe52dca849df928787
Michael Ong <minghao.ong.2014@sis.smu.edu.sg>
2022-10-09 15:38:47 +0800

    fix failing netlify test

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import * as styles from './Blog.styles';
 import { Header } from '../../components';
 
